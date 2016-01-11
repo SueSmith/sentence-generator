b1c833764438c182226b2babfe19507d8315377a
SueSmith <sue@benormal.info>
2016-01-11 20:56:14 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+//sentence generator using express and mongodb
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+//sentence templates array - these make the nonsense text look more like real sentences
+var sentenceTemplates = [];
+//vocab object for part of speech arrays containing words - these populate the templates at random
+var vocab = Object.create(null);
+
+require('./seed.js');//recreate db - just for demo
+
+app.listen(3000, function() {
+    console.log("Listening...");
+});
+
+//app home page
+app.get('/', function(req, res) {
+	//connect to database - url in app configs
+	MongoClient.connect(process.env.DATABASE_URL, function(err, db) {
+		//templates indicate sentence outline using parts of speech, e.g.
+		//determiner noun verb determiner adjective noun
+		var templateCollection = db.collection('sentences');
+		//get the collection as an array
+		templateCollection.find({}).toArray(function(err, templateDocs) {
+			if(err) { console.log(err); res.send("Oops!"); }
+			//loop through and add to array
+			templateDocs.forEach(function(doc) {
+				sentenceTemplates.push(doc.sentence);
+			});
+			//get vocab collection
+			var vocabCollection = db.collection('words');
+			//get word arrays for each part of speech
+			vocabCollection.find({}).toArray(function(err, vocabDocs) {
+				if(err) { console.log(err); res.send("Oops!"); }
+				//loop through and add to array
+				vocabDocs.forEach(function(doc) {
+					vocab[doc.pos] = doc.words;
+				});
+				//close the database connection
+				db.close();
+
+				//try out some sentences and paragraphs
+				res.send("<html><head><style type='text/css'>" +
+					"body, html {width:70%; margin:auto; font-family:sans-serif; background:#000000; color:#ffffff; padding:5%;}" +
+					"</style></head><body>" +
+					"<h1>Sentence Generator DEMO</h1>" +
+					"<p>" + getParagraph(10) + "</p>" +
+					"<h2>" + getSentence() + "</h2>" +
+					"<p>" + getParagraph(6) + "</p>" +
+					"<p>" + getParagraph(8) + "</p>" +
+					"<p><em>Built using MongoDB and Node.js via Compose and Heroku.</em></p>" +
+					"</body></html>");
+				});
+			});
+		});
+});
+
+//helper function to create sentence from data
+function getSentence() {
+	//pick a random template
+	var template = sentenceTemplates[Math.floor(Math.random() * sentenceTemplates.length)];
+	//split the template into parts of speech
+	var sentencePOS = template.split(" ");
+	//create word array
+	var sentenceWords = [];
+	//pick a random word in the correct part of speech for each place in the template
+	sentencePOS.forEach(function(word) {
+		sentenceWords.push(vocab[word][Math.floor(Math.random() * vocab[word].length)]);
+	});
+	var sentence = sentenceWords.join(" ");
+	//make it look more like a normal sentence	
+	return sentence.charAt(0).toUpperCase() + sentence.slice(1, sentence.length) + ".";
+}
+
+//get a specific number of sentences
+function getParagraph(numSentences) {
+	var i;
+	var paragraph = "";
+	for (i = 0; i < numSentences; i++) {
+		paragraph += getSentence() + " ";
+	}
+	return paragraph;
+}
---
+++ seed.js
@@ -0,0 +1,55 @@
+//database client
+var MongoClient = require('mongodb').MongoClient;
+//file reader
+var fs = require('fs');
+
+//seed sentence generator database with data from json files
+
+/*
+Data:
+
+	Sentence templates - these indicate common sentence structures using lists of parts of speech,
+	e.g. The monkey types the lovely code.
+	- structure: determiner noun determiner adjective noun
+	
+	Vocab - lists of words for each part of speech in the sentence templates,
+	e.g. noun: monkey, man, woman, chair, wall
+	- these will be populated into the template structures at random, creating nonsense sentences
+	
+*/
+
+//connect to database
+MongoClient.connect(process.env.DATABASE_URL, function(err, db) {
+    if(err) console.log(err);
+    else {
+			db.collection('sentences').drop(); db.collection('words').drop(); //drop collections - just for demo
+			//insert sentence templates
+			insertData(db, 'sentences', function(sentenceResult){
+				console.log("sentence result: "+sentenceResult);
+				//insert vocab words
+				insertData(db, 'words', function(wordResult){
+					console.log("words result: "+wordResult);
+					db.close();
+				});
+			});
+		}
+});
+
+//helper function for data insertion
+function insertData(db, name, callback){
+	//create collection
+	var newCollection = db.collection(name); 
+	//read from json file
+	fs.readFile(''+name+'.json', 'utf8', function(readErr, data) {
+		if(readErr) console.log(readErr);
+		else{
+			//parse as json
+			var json = JSON.parse(data);
+			//insert as array
+			newCollection.insertMany(json, function(insertErr, docs) {
+				if(insertErr) console.log(insertErr);
+				else callback(docs);
+			});
+		}
+	});
+}
